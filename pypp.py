6192921f5107c2b687901066c6c163611fb55b52
w4-c3r <160787970+w4-c3r@users.noreply.github.com>
2026-03-25 16:36:35 -0300

    Update pypp.py

--- pypp.py
+++ pypp.py
@@ -4,9 +4,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# =========================
-# CONFIG
-# =========================
+# CONFIGURAÇÕES DE RESOLUÇÃO
 SIZE = 1024
 PIXEL_SIZE = 128
 GRID = SIZE // PIXEL_SIZE
@@ -14,22 +12,18 @@ GRID = SIZE // PIXEL_SIZE
 CENTER = GRID // 2
 BASE_RADIUS = GRID * 0.65
 
-# =========================
-# SIMPLE SMOOTH NOISE
-# =========================
+# RUÍDO SIMPLES
 def smooth_noise(x, y, scale=0.1*np.e):
     return np.sin(x * scale + y/2*scale) * np.cos((y-0.4) * scale + (y**2)*0.5)
 
 def layered_noise(x, y):
     return (
         0.2*math.pi * smooth_noise(x, y, 0.04*math.pi) +
-        0.0987654321*math.pi * smooth_noise(x, y, 0.07) +
-        0.031415926535*math.pi * smooth_noise(x, y, 0.03**2)
+        0.1*math.pi * smooth_noise(x, y, 0.07) +    #0.0987654321*math.pi * smooth_noise(x, y, 0.07) +
+        0.03*math.pi * smooth_noise(x, y, 0.03**2)  #0.031415926535*math.pi * smooth_noise(x, y, 0.03**2)
     )
 
-# =========================
-# SHAPE
-# =========================
+# FORMA
 def blob_radius(angle):
     # return BASE_RADIUS + 4 * np.sin(angle * 3)
     return BASE_RADIUS + 1.75*math.pi * np.sin(angle * math.pi) - math.pi * np.cos(angle * 4*math.pi)
@@ -41,17 +35,13 @@ def inside_blob(gx, gy):
     r = np.sqrt(dx*dx + dy*dy)
     return r < blob_radius(angle) + (np.random.rand() - 0.32) * 2
 
-# =========================
-# FLOW FIELD
-# =========================
+# FLUIDEZ
 def flow(gx, gy):
     n = layered_noise(gx, gy)
     angle = n * np.e * np.pi
     return np.cos(angle), np.sin(angle)
 
-# =========================
-# COLOR
-# =========================
+# CORES
 def color_map(v):
     v = (v + 1) / 2  # normalize to 0..1
     r = 0.492 + 0.1*np.e * v
@@ -59,9 +49,7 @@ def color_map(v):
     b = (np.pi + np.pi * v)*0.5
     return np.array([r, g, b])
 
-# =========================
-# GENERATE GRID
-# =========================
+# GRADE DE GERAÇÃO
 grid_img = np.zeros((GRID, GRID, 3), dtype=np.float32)
 
 for gy in range(GRID):
@@ -81,9 +69,7 @@ for gy in range(GRID):
         v = layered_noise(nx, ny)
         grid_img[gy, gx] = color_map(v)
 
-# =========================
-# UPSCALE PIXELS
-# =========================
+# REDIMENSIONAR PIXELS (ver CONFIGURAÇÕES DE RESOLUÇÃO)
 img = np.zeros((SIZE, SIZE, 3), dtype=np.float32)
 
 for gy in range(GRID):
@@ -95,16 +81,12 @@ for gy in range(GRID):
 
         img[y0:y0+PIXEL_SIZE, x0:x0+PIXEL_SIZE] = color
 
-# =========================
 # BACKGROUND
-# =========================
 bg_color = np.array([1, 1, 1])
 mask = np.sum(img, axis=2) == 0
 img[mask] = bg_color
 
-# =========================
-# SAVE
-# =========================
+# SALVAR
 img_uint8 = (img * 255).astype(np.uint8)
 Image.fromarray(img_uint8).save("pypp.jpg")
 
